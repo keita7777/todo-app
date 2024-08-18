125cff4428925f4710b74497e0c78f13a0b9191c
github keita7777 <github mine0kei@gmail.com>
2024-08-18 15:48:58 +0900

    ✨ ログインしたgithubのプロフィール画像を表示できるようにした

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  console.log(user);
+
   return (
     <header className="bg-blue-500 py-2 px-2">
       <div className="max-w-2xl mx-auto flex justify-between items-center">
@@ -35,7 +42,7 @@ const Header = () => {
             </Link>
           </nav>
           <Image
-            src="/profile_icon_default.png"
+            src={user?.image || "/profile_icon_default.png"}
             alt=""
             width={60}
             height={60}
